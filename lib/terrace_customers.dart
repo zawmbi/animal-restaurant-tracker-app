75f3b79576fe1bf08aa7a0cf5a8421da1fe3770e
Linda Mansour <124932177+zawmbi@users.noreply.github.com>
2025-10-19 23:43:35 -0500

    Added terrace customers, But I think that's not all of them?

---
+++ lib/terrace_customers.dart
@@ -0,0 +1,10 @@
+  /// Names for terrace customers. These characters are
+  /// rich and give me lots of money when they have their silly little buffet] 
+  final List<String> terrace_custy = [
+    "Hoppy the Pika",
+    "Bouncy the Pika",
+    "Dobermann",
+    "Sunny",
+    "Mad Hatter"
+  ]
+  
